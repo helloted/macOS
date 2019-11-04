7a53082896f8f6387cf7ee97288807dca71d946f
Haozhicao <helloted@live.com>
2019-11-04 19:34:23 +0800

    update

--- iSandBox/iSandBox/AppDelegate.m
+++ iSandBox/iSandBox/AppDelegate.m
@@ -7,10 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import "GCDAsyncUdpSocket.h"
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <GCDAsyncUdpSocketDelegate>
 
-@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (nonatomic, strong) NSStatusItem *statusItem;
+
+@property (nonatomic, strong)NSMenuItem *ipMenuItem;
 
 @end
 
@@ -18,6 +23,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     [self customStatusItem];
+    [self initUDPSocket];
 }
 
 
@@ -38,7 +44,12 @@
     
     
     NSMenu *menu = [[NSMenu alloc] init];
-    [menu addItemWithTitle:@"Open Feedbin" action:@selector(openFeedbin:) keyEquivalent:@""];
+    
+//    NSString *ip = [self getIPAddress];
+//
+//    [menu addItemWithTitle:[self getIPAddress] action:@selector(openFeedbin:) keyEquivalent:@""];
+    
+    [menu addItem:self.ipMenuItem];
     [menu addItemWithTitle:@"Refresh" action:@selector(getUnreadEntries:) keyEquivalent:@""];
 
     // 灰色分割线
@@ -49,8 +60,52 @@
     _statusItem.menu = menu;
 }
 
+- (void)initUDPSocket{
+    self.ipMenuItem.title = [self getIPAddress];
+}
+
 - (void)openFeedbin:(id)sender{
-    NSLog(@"openFeedbin clicked");
+    NSLog(@"openFeedbin clicked ==%@",[self getIPAddress]);
+}
+
+- (NSString *)getIPAddress {
+
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+
+                }
+
+            }
+
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    freeifaddrs(interfaces);
+    return address;
+
+}
+
+
+#pragma mark Getter&Setter
+
+- (NSMenuItem *)ipMenuItem{
+    if (!_ipMenuItem) {
+        _ipMenuItem = [[NSMenuItem alloc]initWithTitle:@"" action:nil keyEquivalent:@"A"];
+        _ipMenuItem.enabled = YES;
+        _ipMenuItem.alternate = YES;
+    }
+    return _ipMenuItem;
 }
 
 @end
